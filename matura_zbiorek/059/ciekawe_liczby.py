56da8e9c7e32efc6f7c5b89c8f9bde14539b12e4
maniazelazowska <mariazelazowska@protonmail.com>
2023-05-16 13:14:10 +0200

    finish 59

--- matura_zbiorek/059/ciekawe_liczby.py
+++ matura_zbiorek/059/ciekawe_liczby.py
@@ -1,28 +1,75 @@
 import math
+from collections import Counter
 
-temp = open("59/liczby.txt", "r")
+temp = open("liczby.txt")
 numbers = temp.readlines()
 temp.close()
 
-# 59.1
+def uneven_factors(n):
+    uneven_factors = []
+    i = 2
+    while n > 1:
+        if n%i != 0:
+            i+=1
+        else:
+            n//=i
+            if i%2 != 0:
+                uneven_factors.append(i)
+            else:
+                return False
+    if len(set(uneven_factors)) == 3:
+        return True
+    else:
+        return False
+    
+print("59.1")      
+# not optimal, rather brute
 counter = 0
 for number in numbers:
-    number = int(number)
-    num = number
-    unique_factors = []
-    factors = []
-    divider = 2
-    while number >= 2:
-        while number%divider == 0:
-            number //= divider
-            factors.append(divider)
-        divider += 1
-    for factor in factors:
-        if factor not in unique_factors and factor%2 != 0:
-            unique_factors.append(factor)
+    number = int(number.rstrip())
+    if uneven_factors(number):
+        counter += 1
+        
+print(f'{counter}\n)
 
-    if len(unique_factors) == 3:
-        counter = counter + 1
-        print(num, unique_factors)
+print("59.2")
+palindromic_sums = 0
+for number in numbers:
+    number = number.rstrip()
+    number_sum = int(number) + int(number[::-1])
+    if str(number_sum) == str(number_sum)[::-1]:
+        palindromic_sums += 1
+        
+print(f"{palindromic_sums}\n")
+
+print("59.3")
+initial_numbers = []
+powers = []
+max_power1 = 0
+min_power1 = 100009
+for number in numbers:
+    lines = number.rstrip()
+    number = number.rstrip()
+    power = 0 
+    while int(number) > 9:
+        product = 1
+        for char in str(number):
+            product *= int(char)
+        power += 1
+        number = product
+    powers.append(power)
+    initial_numbers.append(int(lines))
+    if power == 1:
+        max_power1 = max(int(lines), max_power1)
+        min_power1 = min(int(lines), min_power1)
+        
+for key, value in Counter(powers).items():
+    print(key, value)
     
-print(counter)
+print(f"Maksymalna dla mocy 1: {max_power1}\nMinimalna: {min_power1}")
+
+    
+    
+        
+
+    
